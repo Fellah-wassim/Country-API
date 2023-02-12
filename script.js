9a39fc68546edfd4893618d576d184ecc713e506
wassim <wbvb41@gmail.com>
2023-02-12 19:26:25 +0100

    added the functionality of displaying countries with the restcountries API

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-"use strict";
+'use strict';
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(request.responseText);
+    console.log(data);
+    const html = `
+    <article class="country">
+            <img class="country__img" src='${data.flags.png}'/>
+            <div class="country__data">
+              <h3 class="country__name">${data.altSpellings[0]}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)}M people</p>
+              <p class="country__row"><span>🗣️</span>${
+                Object.values(data.languages)[0]
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                Object.values(data.currencies)[0].name
+              }</p>
+            </div>
+          </article>
+    `;
+    let htmlObject = document.createElement('div');
+    htmlObject.innerHTML = html;
+    countriesContainer.insertAdjacentElement('afterbegin', htmlObject);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
